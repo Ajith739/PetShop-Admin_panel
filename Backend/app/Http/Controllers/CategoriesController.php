f3e3c992159fb76d3165f7a308c5f7350f003a3d
Ajith <naveenrio739@gmail.com>
2026-05-09 15:39:02 +0530

    categories update,delete

--- Backend/app/Http/Controllers/CategoriesController.php
+++ Backend/app/Http/Controllers/CategoriesController.php
@@ -34,7 +34,42 @@ class categoriesController extends Controller
     // PUT /api/categories/{id}
     public function update(Request $request, $id)
     {
-        $category =categories::find(id)
+        $category = categories::find(id);
+        if (category) {
+            return response()->json([
+                'message' => 'Category not found'
+            ], 404);
+        }
+        $validated = $request->validate([
+            'name' => 'required|unique:categories,name,' . $id,
+            'slug' => 'required|unique:categories,slug,' . $id,
+            'description' => 'nullable|string',
+            'is_active' => 'boolean',
+        ]);
+
+        $category->update($validated);
+
+        return response()->json([
+            'message' => 'Category updated',
+            'data' => $category
+        ]);
+    }
+
+    // DELETE /api/categories/{id}
+    public function destroy($id)
+    {
+        $category = categories::find($id);
+        if ($category) {
+
+            return response()->json([
+                'message' => 'Category not found'
+            ], 404);
+        }
+        $category->delete();
+        return response()->json([
+            'message' => 'Category deleted successfully'
+        ]);
+
     }
 
 }
